b59c7bc15e574d3f6dbc6bffd7a89299d3a883fd
elinekempe <179266@student.talland.nl>
2025-05-16 11:28:24 +0200

    working of PWA

--- public/roulette/schud-detectie.js
+++ public/roulette/schud-detectie.js
@@ -1,6 +1,23 @@
 function enableMotionListener() {
     console.log('Bewegingsdetectie geactiveerd!');
-    
+
+    // iOS vereist expliciete toestemming voor motion events
+    if (typeof DeviceMotionEvent !== 'undefined' && typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    addShakeListener();
+                } else {
+                    alert('Bewegingsdetectie geweigerd.');
+                }
+            })
+            .catch(console.error);
+    } else {
+        addShakeListener();
+    }
+}
+
+function addShakeListener() {
     window.addEventListener('devicemotion', (event) => {
         const acceleration = event.accelerationIncludingGravity;
         const x = acceleration.x;
@@ -8,10 +25,9 @@ function enableMotionListener() {
         const z = acceleration.z;
         console.log(`Acceleratie: x=${x}, y=${y}, z=${z}`);
         if (Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
-            window.location.href = "roulette/schud.html";
+            window.location.href = "/roulette/schud.html"; // Gebruik absoluut pad voor PWA!
         }
     });
-
 }
 
 enableMotionListener();
