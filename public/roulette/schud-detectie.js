efad5468f06c07814a029066e27babe4d64caebb
elinekempe <179266@student.talland.nl>
2025-05-16 11:04:17 +0200

    testing motion

--- public/roulette/schud-detectie.js
+++ public/roulette/schud-detectie.js
@@ -1,4 +1,6 @@
 function enableMotionListener() {
+    console.log('Bewegingsdetectie geactiveerd!');
+    
     window.addEventListener('devicemotion', (event) => {
         const acceleration = event.accelerationIncludingGravity;
         const x = acceleration.x;
@@ -6,10 +8,26 @@ function enableMotionListener() {
         const z = acceleration.z;
         console.log(`Acceleratie: x=${x}, y=${y}, z=${z}`);
         if (Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
-            window.location.href = "schud.html";
+            window.location.href = "roulette/schud.html";
         }
     });
 
 }
 
 enableMotionListener();
+
+setTimeout(() => {
+    const testEvent = new DeviceMotionEvent('devicemotion', {
+        acceleration: {
+            x: 20,
+            y: 0,
+            z: 0
+        },
+        accelerationIncludingGravity: {
+            x: 20,
+            y: 0,
+            z: 0
+        }
+    });
+    window.dispatchEvent(testEvent);
+}, 3000);
