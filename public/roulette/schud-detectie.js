143d7e6d5dc759c0e2136560e109f012282031b3
elinekempe <179266@student.talland.nl>
2025-05-16 10:50:33 +0200

    minder strenge CSP

---
+++ public/roulette/schud-detectie.js
@@ -0,0 +1,13 @@
+function enableMotionListener() {
+    window.addEventListener('devicemotion', (event) => {
+        const acceleration = event.accelerationIncludingGravity;
+        const x = acceleration.x;
+        const y = acceleration.y;
+        const z = acceleration.z;
+        console.log(`Acceleratie: x=${x}, y=${y}, z=${z}`);
+        if (Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
+            window.location.href = "schud.html";
+        }
+    });
+
+}
